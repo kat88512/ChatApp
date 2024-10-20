239a9164da65e209816c166b1132b60396797c46
kat88512 <kasiapost01@gmail.com>
2024-10-20 15:49:03 +0200

    Add packet and packet writer/reader

---
+++ Common/Packets/PacketReader.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Common.Packets
+{
+    public static class PacketReader
+    {
+        private static readonly Encoding _encoding = Encoding.UTF8;
+        private static readonly int _bufferSize = 2 * 1024;
+
+        public static bool TryReadPacket(NetworkStream stream, out Packet? packet)
+        {
+            packet = null;
+
+            try
+            {
+                int bytesRead;
+                byte[] buffer = new byte[_bufferSize];
+
+                var code = stream.ReadByte();
+                bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                if (bytesRead > 0)
+                {
+                    var content = _encoding.GetString(buffer, 0, bytesRead);
+                    packet = new Packet((byte)code, content);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (IOException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Common/Packets/PacketWriter.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Common.Packets
+{
+    public static class PacketWriter
+    {
+        private static readonly Encoding _encoding = Encoding.UTF8;
+
+        public static bool TryWritePacket(NetworkStream stream, Packet packet)
+        {
+            try
+            {
+                var contentAsBytes = _encoding.GetBytes(packet.Content);
+
+                stream.Write([packet.Code], 0, 1);
+                stream.Write(contentAsBytes, 0, contentAsBytes.Length);
+            }
+            catch (IOException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
