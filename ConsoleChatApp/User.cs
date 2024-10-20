38bbf88bbf4e8ea3d252f0c0f9762d11bcac42a1
kat88512 <kasiapost01@gmail.com>
2024-10-20 15:51:21 +0200

    Add user model

---
+++ ConsoleChatApp/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Sockets;
+
+namespace Server
+{
+    internal class User
+    {
+        public Guid Id { get; private init; } = Guid.NewGuid();
+        public string Username { get; private set; }
+        public TcpClient ClientSocket { get; private set; }
+
+        public User(string username, TcpClient clientSocket)
+        {
+            Username = username;
+            ClientSocket = clientSocket;
+        }
+    }
+}
